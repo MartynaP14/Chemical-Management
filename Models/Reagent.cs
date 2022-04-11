54a9876616b2f98de82b4f1ae02a1253a22613a9
Martyna Martyna <martynapacula@gmail.com>
2022-04-11 21:18:55 +0100

    Added attributes to reagent

--- Models/Reagent.cs
+++ Models/Reagent.cs
@@ -1,12 +1,28 @@
-﻿namespace Chemical_Management.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Chemical_Management.Models
 {
     public class Reagent
     {
+        [Key]
+        public int ReagentID { get; set; }
+        [Required]
+        public string ReagentName { get; set; }
 
-        
+        [Required]
+        public int NumberOfVials { get; set; }
 
+        [Required]
+        public string Location { get; set; }
 
+        [Required] //foreign key 
+        public int AnalystID { get; set; }
 
+        [Required]
+        public string ReagentType { get; set;}
 
     }
 }
+
+//Reagent ID, Name, Location, lot number, Quantity (current number of vials), user ID that logged the reagent, Reagent type.
