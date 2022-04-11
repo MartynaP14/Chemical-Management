79c4f556029bb318642ac38fa3563b419406c4e7
Martyna Martyna <martynapacula@gmail.com>
2022-04-11 12:52:40 +0100

    initial commit

---
+++ Chemical-Management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chemical_Management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
