f3ae31022cdd6877a663b242a243a775216a1ac5
Martyna Martyna <martynapacula@gmail.com>
2022-05-04 00:01:29 +0100

    Created test folder , created model test file and added homecontrollers tests

---
+++ Tests/HomeControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chemical_Management.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chemical_Management.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+
+            var logger = Mock.Of<ILogger<HomeController>>();
+
+            var controller = new HomeController(logger);
+
+           
+            var result = controller.Index();
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod()]
+        public void PrivacyTest()
+        {
+
+            var logger = Mock.Of<ILogger<HomeController>>();
+
+            var controller = new HomeController(logger);
+
+
+            var result = controller.Privacy();
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        
+    }
+
+
+
+}
---
+++ Tests/ModelTest.cs
@@ -0,0 +1,35 @@
+﻿using Chemical_Management.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chemical_ManagementTests.Tests
+{
+
+    [TestClass()]
+    public class ModelTest
+    {
+        [TestMethod()]
+        public void CreateReagent()
+        {
+            Reagent r = new Reagent() { ReagentID = 1, ReagentName = HLC, LotNumber = 1002 };
+            Assert.IsInstanceOfType(r, typeof(Reagent));
+            Assert.AreEqual(1, r.ReagentID);
+            Assert.AreEqual(r.ReagentName, "HCL");
+            Assert.AreEqual(r.LotNumber, 1002);
+        }
+
+
+
+        
+
+
+
+
+
+
+    }
+}
